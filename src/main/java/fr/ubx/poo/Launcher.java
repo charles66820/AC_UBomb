157e97326cec6d063d58f46a65277ef2a1257096
charles goedefroit <charles.goedefroit@etu.u-bordeaux.fr>
2021-01-12 00:38:02 +0100

    Add Launcher for jar support

---
+++ src/main/java/fr/ubx/poo/Launcher.java
@@ -0,0 +1,8 @@
+package fr.ubx.poo;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-/*module UBombStudent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens fr.ubx.poo to javafx.fxml;
-    exports fr.ubx.poo;
-}*/
